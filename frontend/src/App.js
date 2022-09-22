45c0c97bd228f6658dea4059b76ad430f1bea5ec
Bahram Jabbarov <bahram.pj@code.local>
2022-09-22 10:18:36 +0400

    categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import DrawerAppBar from './components/navbar/Navbar';
 
 function App() {
-  return <div className="App">salam</div>
+  return (
+    <Routes>
+      <Route path='/' element={<DrawerAppBar/>}>
+        <Route index path='/' element={<div>Home</div>}/>
+        <Route index path='Home' element={<div>Home</div>}/>
+        <Route index path='Products' element={<div>Products</div>}/>
+        <Route index path='Categories' element={<div>Categories</div>}/>
+        <Route index path='Brands' element={<div>Brands</div>}/>
+      </Route>
+    </Routes>
+  )
 }
 export default App
---
+++ frontend/src/components/categories/Categories.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../../utils/axios";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+
+const Categories = () => {
+
+    const [list, setList] = useState([]);
+
+    useEffect(() =>{
+        axiosInstance.get()
+        .then(response => setList(response.data))
+    }, [list])
+
+    return(
+        <>
+        <Box sx={{ bgcolor: '#cfe8fc', p: "5rem"}} align="center">            
+            <Typography sx={{color: '#f542ec'}} align="center" variant="h3" gutterBottom>
+                Categories
+            </Typography>
+            <Table>
+                <TableHead>
+                    <TableRow>                        
+                        <TableCell sx={{fontSize:"32px"}} align="left">ID</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Company Name</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Contact Title</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Contact Name</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Country</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Details</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>                    
+                    {
+                                    
+                        list.map(item => ( 
+                            <TableRow key={item.id}>
+                                <>
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.id}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.companyName}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.contactTitle}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.contactName}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item.address?.country}</TableCell>
+                                    <TableCell>
+                                    <Button key={item} href={`Suppliers/${item.id}`} value={item.id} variant="text" sx={{fontSize:"24px", color: '#1976d2' }}>Details</Button>
+                                    </TableCell>                
+                                </>                        
+                            </TableRow>))                        
+                    }
+                
+                </TableBody>
+            </Table>
+        </Box>
+        </>
+    )
+}
+
+export default Categories;
